59a1fb948ba63eda1416cc715112fe815c9fd928
Dustin Ewers <dustin.ewers@gmail.com>
2019-05-01 17:23:13 -0500

    environment configuration scripts

---
+++ infrastructure/azure-kube-environment.ps1
@@ -0,0 +1,39 @@
+# This script builds an ACR (Azure Container Registry) and a Kubernetes Cluster
+
+
+# You'll need to set these variables.
+$kubernetesResourceGroup="<todo>"
+$acrName='<todo>'
+$aksClusterName='<todo>'
+$location = 'eastus'
+$numberOfNodes = 1 # In production, you're going to want to use at least three nodes.
+
+Write-Host "Creating resource group $resourceGroupName"
+az group create -l $location -n $kubernetesResourceGroup
+
+Write-Host "Creating the Azure Container Registry"
+az acr create --resource-group $kubernetesResourceGroup --name $acrName --sku Standard --location $location
+
+Write-Host "Creating a Service Principal"
+$sp= az ad sp create-for-rbac --skip-assignment | ConvertFrom-Json
+
+$acrID=az acr show --resource-group $kubernetesResourceGroup --name $ACR_NAME --query "id" --output tsv
+
+Write-Host "Assigning the service principal to ACR pull"
+az role assignment create --assignee $sp.appId --scope $acrID --role acrpull
+
+Write-Host "Building a Kubernetes Cluster"
+
+az aks create `
+    --resource-group $kubernetesResourceGroup `
+    --name $aksClusterName `
+    --node-count $numberOfNodes `
+    --service-principal $sp.appId `
+    --client-secret $sp.password `
+    --generate-ssh-keys `
+    --location $location
+
+
+
+
+
---
+++ infrastructure/azure-sql-db.ps1
@@ -0,0 +1,31 @@
+# This script creates a SQL Database
+
+# You'll need to adjust these variables accordingly
+
+$sqlAdminUserName = '<todo>'
+$sqlAdminPassword = '<todo>'
+$sqlServerName = "<todo>"
+$resourceGroupName = "<todo>"
+$sqlDatabaseName = 'customer-db'
+$location = 'eastus'
+
+Write-Host "Creating resource group $resourceGroupName"
+az group create --name $resourceGroupName --location "East US"
+
+Write-Host "Creating SQL Server $sqlServerName"
+az sql server create `
+	--name $sqlServerName `
+	--resource-group $resourceGroupName `
+	--location $location  `
+	--admin-user $sqlAdminUserName `
+	--admin-password $sqlAdminPassword
+
+Write-Host "Creating database $sqlDatabaseName"
+az sql db create `
+	--resource-group $resourceGroupName `
+	--server $sqlServerName `
+	--name $sqlDatabaseName `
+	--service-objective S0
+
+Write-Host "Creating firewall rule..."
+az sql server firewall-rule create -g $resourceGroupName -s $sqlServerName -n "allowAzure" --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
