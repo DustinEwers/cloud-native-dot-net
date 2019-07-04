69b37aacc2290426f60aca64f27630afad959848
Dustin Ewers <dustin.ewers@gmail.com>
2019-07-04 16:07:59 -0500

    Simplified the app deployment. remove all references to MSIs

--- infrastructure/azure-keyvault-setup.ps1
+++
@@ -1,9 +0,0 @@
-
-$resourceGroupName = "customer-data" #this needs to be unique to your subscription
-$keyVaultName = 'customer-app-keyvault'
-$idObjectId="4194ffd3-57b9-466d-a585-b25a161e0149"
-$location = 'eastus'
-
-az keyvault create --name $keyVaultName --resource-group $resourceGroupName --location $location
-
-az keyvault set-policy --name $keyVaultName --object-id $idObjectId --secret-permissions get list
